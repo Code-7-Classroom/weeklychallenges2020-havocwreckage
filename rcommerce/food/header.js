b5910df5960e40069e6c29806f6be7fb78561165
havocwreckage <havoc.wreckage@gmail.com>
2020-03-25 16:09:04 -0400

    WIP

---
+++ rcommerce/food/header.js
@@ -0,0 +1,8 @@
+window.onscroll = () => {
+    console.log(window.scrollY)
+    if(window.scrollY > 0){
+        document.querySelector('#header').classList.add('sticky')
+    }    else{ 
+        document.querySelector('#header').classList.remove('sticky')
+    }
+}
