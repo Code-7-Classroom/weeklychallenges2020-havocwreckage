14f6ddd86a3eab4dd67d16ceea288f884b507177
havocwreckage <havoc.wreckage@gmail.com>
2020-04-13 17:06:43 -0400

    es6

---
+++ Challenge ES6/script.js
@@ -0,0 +1,19 @@
+const me = (name)=>{
+    this.name = "john doe";
+}
+console.log(name);
+
+const sayHi = ()=>{
+    console.log(Hi, ${name});
+}
+
+sayHi();
+// const me = (name, sayHi)=>{
+//     this.name= "Kelvin Melendez";
+//     console.log(name);
+
+//     this.sayHi = ()=>{
+//         console.log(Hi, ${name});
+//     }
+// }
+// console.log(me);
