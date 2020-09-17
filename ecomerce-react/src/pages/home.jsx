b355935f5d2f339c87e3b3ec742ec79ba1fc80cb
havocwreckage <havoc.wreckage@gmail.com>
2020-09-16 22:07:10 -0400

    fix display

--- ecomerce-react/src/pages/home.jsx
+++ ecomerce-react/src/pages/home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Coursel from "../components/coursel/coursel"
-import Intel from '../assets/img/christian-wiediger-c3ZWXOv1Ndc-unsplash.jpg'
-import Amd from '../assets/img/christian-wiediger-Htfy7TaDBgo-unsplash.jpg'
+import Coursel from "../components/coursel/coursel";
+import Intel from "../assets/img/christian-wiediger-c3ZWXOv1Ndc-unsplash.jpg";
+import Amd from "../assets/img/christian-wiediger-Htfy7TaDBgo-unsplash.jpg";
 import "./home.css";
 
 const Home = () => {
@@ -18,22 +18,16 @@ const Home = () => {
         <Coursel />
       </div>
       <div className="only">
-        <div className="container">
+        <div className="container-grid">
           <div className="cell">
             <Link to="/products">
-              <img
-                src={Intel}
-                alt="AMD chipset"
-              />
+              <img src={Intel} alt="AMD chipset" />
               <p>AMD Power Machine</p>
-              </Link>
+            </Link>
           </div>
           <div className="cell">
-          <Link to="/products">
-              <img
-                src={Amd}
-                alt="Intel Chipset"
-              />
+            <Link to="/products">
+              <img src={Amd} alt="Intel Chipset" />
               <p>Intel Power Machine</p>
             </Link>
           </div>
